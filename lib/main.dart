5410715bb37d50999b45c990a1a3a85183504b62
srikrishnakolli <srikrishnakolli0@gmail.com>
2025-02-05 21:56:29 -0500

    Changed background color

--- lib/main.dart
+++ lib/main.dart
@@ -22,65 +22,14 @@ class _PetAppState extends State<PetApp> {
 
   final List<String> petNames = ["Buddy", "Charlie", "Max", "Luna", "Milo"];
 
-  int _currentImageIndex = 0;
-
-  void _changePetImage() {
-    setState(() {
-      _currentImageIndex = (_currentImageIndex + 1) % petImages.length;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Digital Pet')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(petImages[_currentImageIndex], height: 200),
-              const SizedBox(height: 10),
-              Text(
-                petNames[_currentImageIndex],
-                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _changePetImage,
-                child: const Text('Change Pet'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const PetApp());
-}
-
-class PetApp extends StatefulWidget {
-  const PetApp({Key? key}) : super(key: key);
-
-  @override
-  _PetAppState createState() => _PetAppState();
-}
-
-class _PetAppState extends State<PetApp> {
-  final List<String> petImages = [
-    'assets/pet1.jpeg',
-    'assets/pet2.jpeg',
-    'assets/pet3.jpeg',
-    'assets/pet4.jpeg',
-    'assets/pet5.jpeg',
+  final List<Color> bgColors = [
+    Colors.blue[100]!,
+    Colors.green[100]!,
+    Colors.yellow[100]!,
+    Colors.pink[100]!,
+    Colors.purple[100]!
   ];
 
-  final List<String> petNames = ["Buddy", "Charlie", "Max", "Luna", "Milo"];
-
   int _currentImageIndex = 0;
 
   void _changePetImage() {
@@ -93,6 +42,7 @@ class _PetAppState extends State<PetApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: bgColors[_currentImageIndex], // Background color changes
         appBar: AppBar(title: const Text('Digital Pet')),
         body: Center(
           child: Column(
